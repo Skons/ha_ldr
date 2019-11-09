a7f0f0a57bca3f0add86e02b76260032ee302d66
Kevin Temming <k.temming@gmail.com>
2019-11-09 16:43:54 +0100

    Update ha_ldr.py
    Switched to Bearer token

--- ha_ldr.py
+++ ha_ldr.py
@@ -1,11 +1,3 @@
-"""
-@ Authors     : Kevin Temming
-@ Date        : 05-05-2019
-@ Description : HA LDR - Get the LDR value into Home Assistant.
-
-05-05-2019 - First version
-"""
-
 import RPi.GPIO as GPIO
 import time
 import sys
@@ -16,10 +8,11 @@ import argparse
 #parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--pin', action='store', dest='pin', type=int, help="LDR GPIO data pin", default=11)
-parser.add_argument('-m', '--max-value', action='store', dest='max', type=int, help="Max brightness value reported", default=2500)
+parser.add_argument('-m', '--max-value', action='store', dest='max', type=int, help="Max brightness value reported", default=3000)
 parser.add_argument('-l', '--max-loop', action='store', dest='loop', type=int, help="Max number of loops, 0 for indefinate", default=1)
 parser.add_argument('-u', '--url', action='store', dest='url', help="Url to post the value to, if not provided the output is console")
-parser.add_argument('-s', '--sleep', action='store', dest='sleep', help="Number of seconds to wait for next brightness retreival", default=10)
+parser.add_argument('-t', '--token', action='store', dest='token', help="Bearer token to authenticate against your home assistant instance")
+parser.add_argument('-s', '--sleep', action='store', dest='sleep', type=int, help="Number of seconds to wait for next brightness retreival", default=10)
 parser.add_argument('-f', '--log-file', action='store', dest='logfile', help="Log file location")
 parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help="Show verbose logging")
 args = parser.parse_args()
@@ -31,7 +24,7 @@ else:
     loglevel = logging.ERROR
 
 if args.logfile is not None:
-    logging.basicConfig(filename=args.logfile,filemode='w',format='%(asctime)s %(name)s %(levelname)s %(message)s',datefmt='%H:%M:%S',level=loglevel)
+    logging.basicConfig(filename=args.logfile,filemode='a',format='%(asctime)s %(name)s %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',level=loglevel)
 else:
     logging.basicConfig(level=loglevel)
 
@@ -45,7 +38,6 @@ logging.info('Logfile      = %s',args.logfile)
 
 GPIO.setmode(GPIO.BOARD)
 
-#https://pimylifeup.com/raspberry-pi-light-sensor/
 def rc_time (pin_to_circuit,max_value):
     count = 0
   
@@ -69,16 +61,20 @@ def rc_time (pin_to_circuit,max_value):
     return count
 
 def post_rc_time (url, value):
-    headers = {'content-type': 'application/json'}
+    headers = {
+	'Authorization': 'Bearer '+args.token,
+	'content-type': 'application/json'
+}
     payload = {"state":value}
     logging.info('JSON         = %s',payload)
     try:
-        r = requests.post(url, json=payload)
+        r = requests.post(url, json=payload, headers=headers)
         logging.info('Status Code  = %s',r.status_code)
         return r.status_code
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
         logging.error('%s', e)
         return ""
+    #return "ok"
 
 counter = 0 #counter for the while loop
 logging.info('--- initialization done ---')
